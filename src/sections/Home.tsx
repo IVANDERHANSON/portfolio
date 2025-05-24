0b3801db839b26583d7559a839355df2429f1d22
Ivander Hanson Setyawan <hansonivander@gmail.com>
2025-05-24 23:42:27 +0700

    feat: typing text

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -33,9 +33,12 @@ export default function Home({ RefProp }: { RefProp: React.RefObject<HTMLElement
                     <div className="absolute border-b-[50vh] border-l-[55vw] border-l-transparent border-b-blue-800 bottom-0 right-0"></div>
                 </div>
 
-                <div className="w-[50%] h-screen bg-blue-800 flex justify-center items-center flex-col gap-10">
-                    <h1 className="text-white text-[2rem] font-bold">Welcome to my portfolio!</h1>
-                    <h1 className="text-white text-[2rem] font-bold">My name is Ivander Hanson Setyawan</h1>
+                <div className="w-[50%] h-screen bg-blue-800 flex justify-center items-center flex-col gap-10 text-white text-[2rem] font-bold text-center">
+                    <h1>Welcome to my portfolio!</h1>
+                    <h1>My name is Ivander Hanson Setyawan</h1>
+                    <h1>
+                        I'm <span className='relative text-yellow-100 before:content-["a Student"] before:animate-[words_20s_infinite] after:content-[""] after:absolute after:w-[calc(100%+0.5rem)] after:h-[calc(100%+0.5rem)] after:bg-blue-800 after:border-l-[0.3rem] after:border-solid after:border-yellow-100 after:right-[-0.5rem] after:animate-[cursor_0.8s_infinite_,_type_20s_steps(15)_infinite]'></span>
+                    </h1>
                 </div>
             </section>
         </>
