3b110acf260536544dbabc5cb6abe44cefe8159a
Ivander Hanson Setyawan <hansonivander@gmail.com>
2025-05-31 02:02:11 +0700

    feat: update Journey component

--- src/components/Journey.tsx
+++ src/components/Journey.tsx
@@ -15,7 +15,7 @@ export default function Journey({ Props }: { Props: JourneyProps[] }) {
         <>
             <div className="w-[calc(100%-0.6rem)] ml-[0.6rem] flex flex-col gap-[2rem] my-[2rem] pl-[2rem] border-l-[0.3rem] border-solid border-blue-800 border-opacity-[0.1]">
                 {Props.map((Prop, PropIndex) => (<div className="
-                    relative w-full p-[2%] border-[0.05rem] border-solid border-blue-800 border-opacity-[0.2]
+                    relative w-full p-[2%] border-[0.05rem] border-solid border-blue-800 border-opacity-[0.2] bg-blue-800 bg-opacity-[0.1]
                     before:content-[''] before:absolute before:top-[-0.08rem] before:right-[calc(100%+2rem-0.55rem)] before:bg-blue-800 before:w-[1.5rem] before:h-[1.5rem] before:rounded-[50%] before:border-[0.1rem] before:border-solid before:border-white
                 " key={PropIndex}>
                     <div className='w-full flex justify-between'>
